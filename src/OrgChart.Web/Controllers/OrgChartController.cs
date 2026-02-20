d84e04f581ce3692e0f577e47595efce1a540707
gabriel aparecido mizael <gabri@NITROV15>
2026-02-19 21:55:39 -0300

    feat: Add OrgChartController and Razor views for org chart display

---
+++ src/OrgChart.Web/Controllers/OrgChartController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OrgChart.Application.Services;
+
+namespace OrgChart.Web.Controllers;
+
+public class OrgChartController : Controller
+{
+    private readonly IOrgChartService _service;
+
+    public OrgChartController(IOrgChartService service)
+    {
+        _service = service;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var result = await _service.GetOrgChartAsync();
+        if (!result.IsSuccess)
+        {
+            TempData["Error"] = result.ErrorMessage;
+            return View();
+        }
+
+        return View(result.Data);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetOrgChartJson()
+    {
+        var result = await _service.GetOrgChartAsync();
+        if (!result.IsSuccess)
+        {
+            return BadRequest(result.ErrorMessage);
+        }
+
+        return Json(result.Data);
+    }
+}
